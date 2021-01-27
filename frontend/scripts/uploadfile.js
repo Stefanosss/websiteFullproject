97eaa998046794cec74db42855448872b81d07ff
chaimaeBen <chaimae.benhammou@student.ehb.be>
2021-01-27 17:50:14 +0100

    uuid uploadfile

--- frontend/scripts/uploadfile.js
+++ frontend/scripts/uploadfile.js
@@ -2,10 +2,17 @@ $(function () {
   console.log("uploaded file is loaded");
 });
 
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
 $("#uploadForm").submit(function (e) {
 
   console.log('form was submitted')
   e.preventDefault();
+  console.log(uuidv4());
 
     console.log("UPLOAD-FILE called!");
     var storageReference = firebase.storage().ref();

d0aff432888aeaf5a89371592717bd5958f57e08
chaimaeBen <chaimae.benhammou@student.ehb.be>
2021-01-27 14:09:49 +0100

    query params changed

--- backend/routes/remixRoutes.js
+++ backend/routes/remixRoutes.js
@@ -15,56 +15,65 @@ URL = require("url");
 
 //newest
 
+remixRouter.route("/getById").get((req, res) => {
+  //get the detail information of the remix by remix id
+  console.log("id here: " + req.query.id);
+  var ref = firebase.firestore().collection("remixes").doc(req.query.id).get();
 
-  remixRouter
-  .route("/getById")
-  .get((req, res) => {
-    //get the detail information of the remix by remix id
-    console.log('id here: '+  req.query.id);
-    var ref = firebase.firestore().collection("remixes").doc(req.query.id).get();
-
-    ref
-    .then(function (doc) {
-      if (doc.exists) {
-        console.log("Document data:", doc.data());
-      res.send(doc.data())
-      } else {
-        console.log("No such document!");
-      }
-    })
- 
-  })
-
-  remixRouter
-  .route("/getAll")
-  .get((req, res) => {
-    //get the detail information of the remix by remix id
-    const ref = firebase.firestore().collection('remixes').get()
-  
-    ref.then((querySnapshot) => {
-      const data = []
-      querySnapshot.forEach((doc) => {
-         data.push({ id: doc.id, ...doc.data() })
-
-      })
-      res.send(data)
-   }) .catch(function (error) {
-            console.error("Error displaying documents: ", error);
-          });
-
-  })
-
-remixRouter
-  .route("/checkLoggedInUpload")
-  .get((req, res) => {
-    var userId = firebase.auth().currentUser.uid;
-if(userId)
-   { 
-     res.send(userId);
-    }
-    else{
-      res.send("you need to be logged in to upload")
+  ref.then(function (doc) {
+    if (doc.exists) {
+      console.log("Document data:", doc.data());
+      res.send(doc.data());
+    } else {
+      console.log("No such document!");
     }
   });
+});
+
+remixRouter.route("/getAll").get((req, res) => {
+  //get the detail information of the remix by remix id
+  const ref = firebase.firestore().collection("remixes").get();
+
+  ref
+    .then((querySnapshot) => {
+      const data = [];
+      querySnapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+      });
+      res.send(data);
+    })
+    .catch(function (error) {
+      console.error("Error displaying documents: ", error);
+    });
+});
+
+remixRouter.route("/checkLoggedInUpload").get((req, res) => {
+  var userId = firebase.auth().currentUser.uid;
+  if (userId) {
+    res.send(userId);
+  } else {
+    res.send("you need to be logged in to upload");
+  }
+});
+
+sampleRouter.get("/downloadById/:id", (req, res) => {
+  //download sa marche
+
+  const storageRef = firebase.storage().ref();
+
+  //get id from url
+  //get name query with id
+
+  var ref = firebase.firestore().collection("remixes").doc(req.params.id).get();
+
+  ref.then(function (doc) {
+    if (doc.exists) {
+      console.log("Document data:", doc.data());
+      res.send(doc.data());
+    } else {
+      console.log("No such document!");
+    }
+  });
+});
 
 module.exports = remixRouter;

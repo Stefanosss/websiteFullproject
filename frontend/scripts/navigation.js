ae266020680974258d70c1e05000355847dc905b
Stefanosss <stefanos.stoikos@student.ehb.be>
2021-01-28 15:44:17 +0100

    changed some frontend details

--- frontend/scripts/navigation.js
+++ frontend/scripts/navigation.js
@@ -1,52 +1,35 @@
-
-  async function getUserInfo(id) {
-    let response = await fetch(
-      "https://fullproject-backend.herokuapp.com/authentication/getById/"+id,
-      { mode: "cors" }
-    );
-    return await response.json();
-  }
-
-
+async function getUserInfo(id) {
+  let response = await fetch("https://fullproject-backend.herokuapp.com/authentication/getById/" + id, { mode: "cors" });
+  return await response.json();
+}
 
 window.onload = () => {
-
-  
   console.log("href " + window.location.search);
   var url = window.location.search;
   url = url.replace("?id=", "");
   console.log(url);
 
-  async function runAll(url){
+  async function runAll(url) {
+    console.log("entered");
+    if (url) {
+      const [user] = await Promise.all([getUserInfo(url)]);
 
-    console.log('entered')
-  if (url) {
-    const [user] = await Promise.all([
-      getUserInfo(url),
- 
-    ]);
-   
-     console.log("a user "+user);
-    $(".user-nav").append(`
+      console.log("a user " + user);
+      $(".user-nav").append(`
     <a href="https://fullproject-frontend.herokuapp.com/views/home.html" class="user-nav__link">Home</a>
     <a href="sampleGenerator.html" class="user-nav__link">Generate sample</a>
     <div class="user-nav__user">
         <p class="user-nav__user-name">${user.firstname} ${user.lastname}</p>
-        
         <img src="img/userPicture.jpg" alt="user picture" class="user-nav__user-photo">
-    </div>`)
-  }else{
-    $(".user-nav").append(`
-    <a href="https://fullproject-frontend.herokuapp.com/views/home.html" class="user-nav__link">Home</a>
-    <a href="sampleGenerator.html" class="user-nav__link">Generate sample</a>
-    <div class="user-nav__user">
-        <a href="https://fullproject-frontend.herokuapp.com/views/login.html" class="user-nav__user-name">Login</a> <span></span>
-        <a  href="https://fullproject-frontend.herokuapp.com/views/signup.html" class="user-nav__user-name">Sign up</a>
-
-    </div>`)
+    </div>`);
+    } else {
+      $(".user-nav").append(`
+        <a href="https://fullproject-frontend.herokuapp.com/views/home.html" class="user-nav__link">Home</a>
+        <a href="sampleGenerator.html" class="user-nav__link">Generate sample</a>
+        <a href="https://fullproject-frontend.herokuapp.com/views/login.html" class="user-nav__link">Login</a>
+        <a  href="https://fullproject-frontend.herokuapp.com/views/signup.html" class="user-nav__link">Sign up</a>
+    `);
+    }
   }
-}
-runAll();
-
-
-  }
+  runAll();
+};
